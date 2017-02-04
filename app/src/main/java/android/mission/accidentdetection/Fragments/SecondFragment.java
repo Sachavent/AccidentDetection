d79b6b37613035e3860140aa3a8dba53f93c3a40
Tanguy <demme.tanguy@hotmail.fr>
2017-02-05 00:10:55 +0100

    buged version of search

--- app/src/main/java/android/mission/accidentdetection/Fragments/SecondFragment.java
+++ app/src/main/java/android/mission/accidentdetection/Fragments/SecondFragment.java
@@ -12,11 +12,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -33,8 +36,6 @@ public class SecondFragment extends Fragment {
     private Button butonAddContact;
     private HashMap<String, String> contactList;
     private HashMap<String, String> contactsJustAdded;
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,8 +72,6 @@ public class SecondFragment extends Fragment {
 
         GetterContactsPhone getterContactsPhone = new GetterContactsPhone(getContext());
         contactList = getterContactsPhone.getAllContact();
-
-
 
         //Log.d("hashmap", "floriane: " + contactList.get("Floriane"));
     }
